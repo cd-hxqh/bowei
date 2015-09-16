fd21c8b4d77847f0d894c4c9ec8a18613c1bd2c5
taocaocajaini <278233503@qq.cpm>
2015-09-16 17:51:29 +0800

    退出功能

--- app/src/main/java/com/cdhxqh/bowei/ui/activity/MainHomeActivity.java
+++ app/src/main/java/com/cdhxqh/bowei/ui/activity/MainHomeActivity.java
@@ -1,7 +1,9 @@
 package com.cdhxqh.bowei.ui.activity;
 
+import android.app.AlertDialog;
 import android.app.FragmentTransaction;
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
@@ -42,7 +44,9 @@ import java.util.Date;
 public class MainHomeActivity extends BaseActivity {
     public static final String TAG = "MainHomeActivity";
 
-    /**搜索按钮**/
+    /**
+     * 搜索按钮*
+     */
     private ImageView searchImageView;
     private ListView mLvLeftMenu;
     private DrawerLayout mDrawerLayout;
@@ -50,22 +54,29 @@ public class MainHomeActivity extends BaseActivity {
     private TextView titlename;
     private FragmentTransaction fragmentTransaction;
     private OrderFragment orderFragment = new OrderFragment(); //工单
-    /**库存**/
-    private InventoryFragment inventoryFragment=new InventoryFragment();
-    /**知识库**/
-    private KnowKedge_Fragment knowKedge_Fragment=new KnowKedge_Fragment();
+    /**
+     * 库存*
+     */
+    private InventoryFragment inventoryFragment = new InventoryFragment();
+    /**
+     * 知识库*
+     */
+    private KnowKedge_Fragment knowKedge_Fragment = new KnowKedge_Fragment();
     private DownloadFragment downloadFragment = new DownloadFragment();
-    /**资产扫描**/
-    private AssetFragment assetFragment=new AssetFragment();
+    /**
+     * 资产扫描*
+     */
+    private AssetFragment assetFragment = new AssetFragment();
 
-    /**设置界面**/
-    private SettingFragment settingFragment=new SettingFragment();
+    /**
+     * 设置界面*
+     */
+    private SettingFragment settingFragment = new SettingFragment();
 
 
     private ProgressDialog mProgressDialog;
     TextView nameview;
     TextView dateview;
-
 
 
     @Override
@@ -85,7 +96,7 @@ public class MainHomeActivity extends BaseActivity {
         mLvLeftMenu = (ListView) findViewById(R.id.left_menu);
         maintitleimg = (ImageView) findViewById(R.id.main_title);
         titlename = (TextView) findViewById(R.id.title_activity_name);
-        searchImageView=(ImageView)findViewById(R.id.title_search_btn);
+        searchImageView = (ImageView) findViewById(R.id.title_search_btn);
     }
 
     @Override
@@ -94,7 +105,7 @@ public class MainHomeActivity extends BaseActivity {
         invalidateOptionsMenu();
         fragmentTransaction = getFragmentManager().beginTransaction();
         fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
-        fragmentTransaction.replace(R.id.container,orderFragment);
+        fragmentTransaction.replace(R.id.container, orderFragment);
         fragmentTransaction.commit();
         mLvLeftMenu.setOnItemClickListener(leftmenulistener);
         maintitleimg.setOnClickListener(new View.OnClickListener() {
@@ -107,12 +118,11 @@ public class MainHomeActivity extends BaseActivity {
         });
     }
 
-    private void setUpDrawer()
-    {
+    private void setUpDrawer() {
         LayoutInflater inflater = LayoutInflater.from(this);
         View view = inflater.inflate(R.layout.header_username, mLvLeftMenu, false);
         nameview = (TextView) view.findViewById(R.id.id_link);
-        nameview.setText(Constants.UserName+",你好");
+        nameview.setText(Constants.UserName + ",你好");
         dateview = (TextView) view.findViewById(R.id.id_date);
         dateview.setText(new SimpleDateFormat("yyyy.MM.dd").format(new Date()));
         mLvLeftMenu.addHeaderView(view);
@@ -124,25 +134,25 @@ public class MainHomeActivity extends BaseActivity {
         public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
             mDrawerLayout.closeDrawer(Gravity.LEFT);
             searchImageView.setVisibility(View.GONE);
-            switch (position){
+            switch (position) {
                 case 1://工单管理
                     titlename.setText(getResources().getString(R.string.order));
                     fragmentTransaction = getFragmentManager().beginTransaction();
                     fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
-                    fragmentTransaction.replace(R.id.container,orderFragment);
+                    fragmentTransaction.replace(R.id.container, orderFragment);
                     fragmentTransaction.commit();
                     break;
                 case 2://资产查询
                     fragmentTransaction = getFragmentManager().beginTransaction();
                     fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
-                    fragmentTransaction.replace(R.id.container,assetFragment);
+                    fragmentTransaction.replace(R.id.container, assetFragment);
                     fragmentTransaction.commit();
                     titlename.setText(getResources().getString(R.string.property));
                     break;
                 case 3://库存查询
                     fragmentTransaction = getFragmentManager().beginTransaction();
                     fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
-                    fragmentTransaction.replace(R.id.container,inventoryFragment);
+                    fragmentTransaction.replace(R.id.container, inventoryFragment);
                     fragmentTransaction.commit();
                     titlename.setText(getResources().getString(R.string.inventory));
                     break;
@@ -151,7 +161,7 @@ public class MainHomeActivity extends BaseActivity {
                     searchImageView.setOnClickListener(searchOnClickListener);
                     fragmentTransaction = getFragmentManager().beginTransaction();
                     fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
-                    fragmentTransaction.replace(R.id.container,knowKedge_Fragment);
+                    fragmentTransaction.replace(R.id.container, knowKedge_Fragment);
                     fragmentTransaction.commit();
                     titlename.setText(getResources().getString(R.string.knowledge));
                     break;
@@ -170,13 +180,52 @@ public class MainHomeActivity extends BaseActivity {
                     titlename.setText(getResources().getString(R.string.setting));
                     break;
                 case 7://退出
-                    AppManager.AppExit(MainHomeActivity.this);
+                    showDialog();
+
                     break;
             }
         }
     };
 
-    private void getOwnerId(){
+
+    /**
+     * 弹出对话框*
+     */
+
+    private void showDialog() {
+
+        if (!mDrawerLayout.isDrawerOpen(Gravity.LEFT)) {
+            mDrawerLayout.openDrawer(Gravity.LEFT);
+        }
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainHomeActivity.this);
+
+        builder.setTitle("提示");
+        builder.setMessage("确定退出程序");
+        builder.setPositiveButton("确定", new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface arg0, int arg1) {
+                AppManager.AppExit(MainHomeActivity.this);
+            }
+        });
+
+        builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface arg0, int arg1) {
+
+            }
+        });
+
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+
+
+    }
+
+
+    private void getOwnerId() {
         mProgressDialog = ProgressDialog.show(this, null,
                 getString(R.string.requesting), true, true);
         HttpManager.getData(this, Constants.getOwnerId(getBaseApplication().getUsername()), new HttpRequestHandler<String>() {
@@ -205,6 +254,7 @@ public class MainHomeActivity extends BaseActivity {
             }
         });
     }
+
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
 
@@ -219,15 +269,14 @@ public class MainHomeActivity extends BaseActivity {
 
 
     /****/
-    private View.OnClickListener searchOnClickListener=new View.OnClickListener() {
+    private View.OnClickListener searchOnClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            Intent intent=new Intent();
-            intent.setClass(MainHomeActivity.this,SearchActivity.class);
-            startActivityForResult(intent,0);
+            Intent intent = new Intent();
+            intent.setClass(MainHomeActivity.this, SearchActivity.class);
+            startActivityForResult(intent, 0);
         }
     };
-
 
 
 }
